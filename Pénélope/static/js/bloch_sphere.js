feba7c566a3c1023150a0927d28b24ca15bb9974
Lemniscate <Lemniscate_zero@proton.me>
2024-07-29 03:57:08 +0000

    Updates

--- Pénélope/static/js/bloch_sphere.js
+++
@@ -1,6 +0,0 @@
-function createBlochSphere() {
-    const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
-    const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    scene.add(sphere);
-}
